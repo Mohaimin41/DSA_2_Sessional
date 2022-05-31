c0fe37a8050b9bde7d7fb2dfce9595ffecf452a7
Mohaimin41 <1905041@ugrad.cse.buet.ac.bd>
2022-05-31 18:49:24 +0600

    works ish, THANKS SAHIL ❤

--- graph3_mst/graph.h
+++ graph3_mst/graph.h
@@ -1,7 +1,10 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 #include <algorithm>
+#include <climits>
 
+#define INF INT_MAX
 #define mp(a, b) std::make_pair(a, b)
 #define edge_dec std::pair<int, int>
 
@@ -13,6 +16,9 @@ enum algo
 
 class graph
 {
+    // adjacency list, array of vector of <next vertex, weight of this edge>
+    std::vector<std::pair<int, double>> *Gr;
+
     // graph edge list, <weight, edge: a pair of vertices>
     std::vector<std::pair<double, edge_dec>> gr;
 
@@ -23,10 +29,15 @@ class graph
     // parent array, defines which set currently the vertice belongs to, initialized as own set only
     int *par;
 
+    // vistitation arr for Prim's
+    bool *isVisited;
+
+    // key arr for Prim's
+    double *key;
+
     // num of nodes
     int V;
 
-    
     // finds which tree/set the vertex is in, O(V)
     int FIND_SET(int vertex)
     {
@@ -45,33 +56,51 @@ class graph
         par[u] = par[v];
     }
 
+    void init()
+    {
+        for (int i = 0; i < V; i++)
+        {
+            isVisited[i] = false;
+            par[i] = i;
+            key[i] = INF;
+        }
+        kruskal_tr.clear();
+    }
 
 public:
     graph(int V)
     {
         this->V = V;
-        par = new int[V];
 
-        // MAKE_SET step, O(V)
-        for (int i = 0; i < V; i++)
-            par[i] = i;
+        Gr = new std::vector<std::pair<int, double>>[V];
+        key = new double[V];
+        par = new int[V];
+        isVisited = new bool[V];
+
+        // MAKE_SET step, O(V), also  isVisited
+        init();
 
         gr.clear();
-        kruskal_tr.clear();
     }
 
     ~graph()
     {
+        delete[] key;
         delete[] par;
+        delete[] isVisited;
+        delete[] Gr;
     }
 
     void addEdge(int u, int v, int weight)
     {
         gr.push_back(mp(weight, mp(u, v)));
+        Gr[u].push_back(mp(v, weight));
+        Gr[v].push_back(mp(u, weight));
     }
 
     void mst_kruskal()
     {
+        init();
         // sorting the edge list by weight in ascending order, O(E log E)
         std::sort(gr.begin(), gr.end());
 
@@ -89,20 +118,82 @@ public:
         }
     }
 
-    int cost_mst()
+    void mst_prim()
     {
-        mst_kruskal();
-        int res = 0;
 
-        for (int i = 0; i < kruskal_tr.size(); i++)
-            res += kruskal_tr[i].first;
+        init();
 
+        // min priority queue, sorted over first value in pair,
+        std::priority_queue<std::pair<double, int>,
+                            std::vector<std::pair<double, int>>,
+                            std::greater<std::pair<double, int>>>
+            pq;
+
+        // random src, 0 th ver, pushed with 0 key value, will stay at top now
+        pq.push(mp(0, 0));
+        key[0] = 0;
+
+        //this is executed till all vertices have been pushed then popped or V times
+        //with O(lg V) extraction, O(V lg V)
+        // sum total = O(V lg V) + O(E lg V)
+        while (!pq.empty())
+        {
+            // extract top/min keyed vertex u, O(lg V)
+            int u = pq.top().second;
+            pq.pop();
+
+            // while iterating over adjacency list, same vertex may be added multiple times, so ignore visited vertex
+            if (!isVisited[u])
+            {
+                isVisited[u] = true;
+
+                // iterate through all adjacent of u
+                // total adjacency list entry is 2 * |E| times, or total time for loop ran
+                // on average then, O(E/V)
+                // and total run time is in O(E * lg V)
+                for (std::vector<std::pair<int, double>>::iterator it = Gr[u].begin();
+                     it != Gr[u].end(); it++)
+                {
+                    int v = (*it).first;
+                    double weight = (*it).second;
+
+                    // if v is not visited AND it's key is greater than weight of edge connecting it to tree,
+                    // we add it to pq, and update it's key and set u as parent
+                    if (!isVisited[v] && key[v] > weight)
+                    {
+                        key[v] = weight;
+                        // insertion in min priority queue O(lg V)
+                        pq.push(mp(key[v], v));
+                        par[v] = u;
+                    }
+                }
+            }
+        }
+    }
+
+    double cost_mst(algo a)
+    {
+        double res = 0;
+        if (a == KRUSKAL)
+        {
+            mst_kruskal();
+
+            for (int i = 0; i < kruskal_tr.size(); i++)
+                res += kruskal_tr[i].first;
+        }
+        else
+        {
+            mst_prim();
+            for (int i = 0; i < V; i++)
+            {
+                res += key[i];
+            }
+        }
         return res;
     }
 
     void print_mst(algo a)
     {
-
         if (a == KRUSKAL)
         {
             mst_kruskal();
@@ -116,6 +207,19 @@ public:
             }
 
             std::cout << "}";
+        }
+
+        else
+        {
+            mst_prim();
+            std::cout << "List of edges selected by Prim's: {";
+
+            for (int i = 1; i < V; i++)
+            {
+                std::cout << "(" << par[i] << "," << i << ")";
+                if (i < V - 1)
+                    std::cout << ",";
+            }
         }
     }
 };
--- graph3_mst/main.cpp
+++ graph3_mst/main.cpp
@@ -4,48 +4,33 @@
 
 int main()
 {
-    // graph test(4);
-    // test.addEdge(0,1,3);
-    // test.addEdge(1,2,8);
-    // test.addEdge(2,3,9);
-    // test.addEdge(3,1,6);
-    // std::freopen("mst.txt", "r", stdin);
-
     //file input part
     std::ifstream inputFileStream("mst.txt", std::ios::in);
-    std::streambuf *stream_buffer_file_in = inputFileStream.rdbuf();
-
-    // Backup streambuffers of cin cout
-    std::streambuf *stream_buffer_cin = std::cin.rdbuf();
-
-    //redirection part
-    std::cin.rdbuf(stream_buffer_file_in);
-
-
+    
     int ver, edge;
-    std::cin >> ver >> edge;
-    std::cout <<"v: " << ver << "E: "  << edge << "\n";
+    inputFileStream >> ver >> edge;
+    
+    // std::cout <<"v: " << ver << "E: "  << edge << "\n";
 
     graph t(ver);
 
     for (int i = 0; i < edge; i++)
     {
-        int u, v, weight;
-        std::cin >> u >> v >> weight;
+        int u, v;
+        double weight;
+        inputFileStream >> u >> v >> weight;
 
         
-        std::cout << "U: " << u << "v: " << v << "w: " << weight << "\n";
+        // std::cout << "U: " << u << " v: " << v << " w: " << weight << "\n";
 
         t.addEdge(u, v, weight);
     }
-
-    // sets cin streambuffer and returns the old
-    std::cin.rdbuf(stream_buffer_cin);
+    
     //close file
     inputFileStream.close();
     
 
-    std::cout << "Cost of the minimum spanning tree: " << t.cost_mst() << "\n";
-    t.print_mst(KRUSKAL);
+    std::cout << "Cost of the minimum spanning tree: " << t.cost_mst(PRIM) << "\n";
+    t.print_mst(PRIM);
     return 0;
 }
